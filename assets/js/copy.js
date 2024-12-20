b03188c4d44cc56779d717df6bd532a7bfee2546
Mansoor Barri <90044118+mansoorbarri@users.noreply.github.com>
2024-12-20 06:54:19 +0000

    add: copy-code button

---
+++ assets/js/copy.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const copyButtons = document.querySelectorAll(".copy-btn");
+  
+    copyButtons.forEach(button => {
+      button.addEventListener("click", () => {
+        const codeBlock = button.nextElementSibling.querySelector("code");
+        const codeText = codeBlock.textContent;
+  
+        navigator.clipboard.writeText(codeText).then(() => {
+          button.textContent = "Copied!";
+          setTimeout(() => {
+            button.textContent = "Copy";
+          }, 2000);
+        }).catch(err => {
+          console.error("Failed to copy text: ", err);
+        });
+      });
+    });
+  });
+  
